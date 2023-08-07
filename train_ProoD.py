6f0976badc2811fff70aaf62f5bd6ee77850c8f8
jdey4 <jdey4@jhmi.edu>
2023-08-07 14:00:27 -0400

    LeNet implemented

--- train_ProoD.py
+++ train_ProoD.py
@@ -39,6 +39,7 @@ def main(args):
             print(pretty_dump_yaml(config, 0, sep=' '))
             writer.add_text('Hyperparameters', config_str)
             
+            print('Going in!!!               vssw')
             model = fac.get_model(args.architecture)
             if type(args.gpu)==omegaconf.listconfig.ListConfig:
                 device = torch.device('cuda:' + str(min(args.gpu)))
